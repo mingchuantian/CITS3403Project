b67573cd2c276ddcb05a029ad0cc0bffc39a4043
ming <1120509786@qq.com>
2020-04-29 06:21:41 -0700

    Added Flask Database Migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask     #flask is module and Flask is a class
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #initialize a Flask object, and assign to 'app'
 #app now is a Flask object
@@ -11,6 +12,11 @@ app.config.from_object(Config)
 
 #initialize database object
 db = SQLAlchemy(app)
+
+#initialize Flask-Migrate
+migrate = Migrate(app, db)
+
+
 
 from app import routes, models      #app is a folder name and there is route.py
 
