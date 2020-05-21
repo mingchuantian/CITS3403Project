cfd24e1dab74c0498f7e6b61c07cc2ef9938999a
danielzhang1998 <63471153+danielzhang1998@users.noreply.github.com>
2020-05-22 04:27:07 +0800

    function check quiz create/answer finished or not

---
+++ app/static/quiz_edit_submit.js
@@ -0,0 +1,16 @@
+function submit_new_quiz(){
+    if(document.getElementsByClassName("quiz").length > 0){
+        alert('The quiz creating is not finished already, you can not submit it!');
+    }else{
+        alert('you finished all the quiz question creating!');
+    }
+}
+
+function submit_quiz_answer(){
+    if(document.getElementsByClassName("quiz").length > 0){
+        alert('You did not finish all questions, please do not submit it!');
+    }else{
+        alert('you finished all the quiz question, now submit it!');
+        document.getElementsByTagName('button')[2].click();
+    }
+}
